284a1af21da65dfe199ee2fb31769c44293eb7d6
mariapikhtovnikava22 <pikhtovnikava_maria@mail.ru>
2023-02-28 18:22:17 +0300

    Add sorting numbers by parity

--- pythonLab1/function.py
+++ pythonLab1/function.py
@@ -1,2 +1,16 @@
 def Hello_World():
     print("Hello World")
+
+def parity (lis):
+    new_list = list()
+
+    for i in lis:
+        if i % 2 == 0:
+            new_list += [i]
+
+    print(new_list)
+
+
+
+
+
--- pythonLab1/main.py
+++ pythonLab1/main.py
@@ -1,3 +1,16 @@
 from function import*
 
 Hello_World()
+
+while True:
+    try:
+        arr = []
+        for i in input("PLease, enter a list of number: ").split():
+            arr.append(int(i))
+        break
+    except ValueError:
+        print("ERROR, you didn't enter a number!!!")
+
+parity(arr)
+
+
